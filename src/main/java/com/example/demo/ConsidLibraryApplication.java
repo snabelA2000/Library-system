72a69ccbe754fa17eeb4c0c01a1b1de9fb023b8f
snabelA00 <ja000411@gmail.com>
2022-10-03 19:38:56 +0200

    spring initialized

---
+++ src/main/java/com/example/demo/ConsidLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsidLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsidLibraryApplication.class, args);
+	}
+
+}
