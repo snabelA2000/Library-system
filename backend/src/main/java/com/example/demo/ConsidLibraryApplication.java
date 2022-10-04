2270e7445ad3c6b44ceadc4d1a4ca5923b815d96
snabelA00 <ja000411@gmail.com>
2022-10-04 12:02:31 +0200

    sout test

--- backend/src/main/java/com/example/demo/ConsidLibraryApplication.java
+++ backend/src/main/java/com/example/demo/ConsidLibraryApplication.java
@@ -3,13 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.gson.GsonProperties;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 @SpringBootConfiguration
 public class ConsidLibraryApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConsidLibraryApplication.class, args);
+
+		System.out.println("Test");
 	}
 
 }
